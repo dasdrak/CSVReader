a94fbc940839c582fdbcc48414d79f4fb786723b
Dasdrak <salva6670666@hotmail.com>
2022-09-26 12:34:56 +0200

    added opencsv + read csv files

--- prueba/src/main/java/com/pruebamaven/CommandArgs.java
+++ prueba/src/main/java/com/pruebamaven/CommandArgs.java
@@ -1,26 +1,64 @@
 package com.pruebamaven;
 
-import java.util.Scanner;
-import java.util.regex.Pattern;
+
+import java.io.*;
+import java.net.*;
+
+
+import com.opencsv.exceptions.CsvException;
 
 public class CommandArgs{
-	/**
+	protected String url;
+
+    /**
 	 * 
 	 * @param args the command line arguments
+     * @throws IOException
+     * @throws FileNotFoundException
+     * @throws CsvException
 	 */
-    public static void main(String[] args) {
-        System.out.println("Por favor introduzca la URL: ");
+    public static void main(String[] args) throws FileNotFoundException, IOException, CsvException {
+   
+        Console console = System.console();
+        String fileRoute = console.readLine("Por favor introduzca la URL: ");
 
-        Scanner url = new Scanner(System.in);
+        
 
-        if (url.hasNext(Pattern.compile(".*.csv.*")))
+        if (fileRoute.contains(".csv")||fileRoute.startsWith("http")||fileRoute.startsWith("www")){
             System.out.println("La URL introducida es correcta");
-        else
+            System.out.println("cargando "+fileRoute);
+
+
+            if (fileRoute.startsWith("http")||fileRoute.startsWith("www")){
+                URL oracle = new URL(fileRoute);
+                BufferedReader in = new BufferedReader(
+                new InputStreamReader(oracle.openStream()));
+
+            String inputLine;
+                while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+            in.close();
+            }
+            if (fileRoute.contains(".csv")){
+                URL oracle = new URL("file:///"+fileRoute);
+                BufferedReader in = new BufferedReader(
+                new InputStreamReader(oracle.openStream()));
+
+            String inputLine;
+                while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+            in.close();
+            }  
+        }
+        else 
             System.out.println("La URL introducida no es correcta, asegurate de que la extensión sea .csv");
 
-        if (url != null)
-            url.close();
-
+        
+       // if (sc != null)
+         //   sc.close();
     }
+    public String GetUrl(){
+        return url;
+    };
 
-}
+};
---
+++ prueba/src/main/java/com/pruebamaven/Main.java
@@ -0,0 +1,23 @@
+package com.pruebamaven;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.opencsv.exceptions.CsvException;
+
+/**
+ * 
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args ) throws FileNotFoundException, IOException, CsvException
+    {
+     CommandArgs.main(args);
+    
+    }
+
+}
+
+
