5ed2f2a1cd1f1df3054a3b0280f12771b13a3b78
Dasdrak <salva6670666@hotmail.com>
2022-10-04 14:58:51 +0200

    updated string names

--- prueba/src/main/java/com/CSVloader/PruebaApplication.java
+++ prueba/src/main/java/com/CSVloader/PruebaApplication.java
@@ -10,16 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
-public class PruebaApplication implements CommandLineRunner{
-	
+public class PruebaApplication implements CommandLineRunner {
+
 	public static void main(String[] args) throws FileNotFoundException, IOException, SQLException {
 		SpringApplication.run(PruebaApplication.class, args);
 		CSVRead.main(args);
 	}
+
 	@Override
 	public void run(String... args) throws Exception {
-		
-		
+
 	}
 
 }
--- prueba/src/main/java/com/CSVloader/Utils.java
+++ prueba/src/main/java/com/CSVloader/Utils.java
@@ -10,93 +10,88 @@ import org.apache.commons.collections.CollectionUtils;
 
 public class Utils {
 
-    public static Connection connection() throws SQLException{
+    public static Connection connection() throws SQLException {
         Connection connection = null;
-    
+
         String jdbcURL = "jdbc:mysql://localhost:3306/db_test";
         String username = "springuser";
         String password = "ThePassword";
-    
+
         connection = DriverManager.getConnection(jdbcURL, username, password);
         connection.setAutoCommit(false);
 
         System.out.println("conected to MySQL");
-        
-       
+
         return connection;
     }
-    
+
     public static List<String> selectDNI(Connection connection) throws SQLException {
 
         Statement statement = connection.createStatement();
-        ResultSet rs = statement.executeQuery ("select * from userdata");
+        ResultSet rs = statement.executeQuery("select * from userdata");
         List<String> rslist = new ArrayList<String>();
 
-            while (rs.next())
-                {
-                String dniSQL = rs.getString(4);
-                rslist.add(dniSQL);
-                    }
-    
+        while (rs.next()) {
+            String dniSQL = rs.getString(4);
+            rslist.add(dniSQL);
+        }
+
         return rslist;
-        
-        
+
     }
-    public static void insert( String url, Connection connection) throws SQLException, IOException{
-        
+
+    public static void insert(String url, Connection connection) throws SQLException, IOException {
+
         String sql = "INSERT INTO userdata (Id, FirstName, LastName, DNI, Birthdate, Email) VALUES (?, ?, ?, ?, ?, ?)";
         PreparedStatement insertstatement = connection.prepareStatement(sql);
-      
-    
+
         BufferedReader in = new BufferedReader(new FileReader(url));
-            String lineText = null;
-            
-            long lines = 0;
-            in.readLine();
-    
-            while ((lineText = in.readLine()) != null) {
-    
-    
-                String[] data = lineText.split(";");
-                lines++;
-                String Id = data[0];
-                String FirstName = data[1];
-                String LastName = data[2];
-                String DNI = data[3];
-                String Birthdate = data[4];
-                String Email = data[5];
-                
-                if (!(compareDni(lineText, connection))){
-                
-                    insertstatement.setString(1, Id);
-                    insertstatement.setString(2, FirstName);
-                    insertstatement.setString(3, LastName);
-                    insertstatement.setString(4, DNI);
-                    insertstatement.setString(5, Birthdate);
-                    insertstatement.setString(6, Email);
-                    System.out.println(lineText);
-                
-                    insertstatement.addBatch();
-                    insertstatement.executeBatch();
-                }
-                System.out.println("Total read rows = "+ lines);
+        String lineText = null;
+
+        long lines = 0;
+        in.readLine();
+
+        while ((lineText = in.readLine()) != null) {
+
+            String[] data = lineText.split(";");
+            lines++;
+            String Id = data[0];
+            String FirstName = data[1];
+            String LastName = data[2];
+            String DNI = data[3];
+            String Birthdate = data[4];
+            String Email = data[5];
+
+            if (!(compareDni(lineText, connection))) {
+
+                insertstatement.setString(1, Id);
+                insertstatement.setString(2, FirstName);
+                insertstatement.setString(3, LastName);
+                insertstatement.setString(4, DNI);
+                insertstatement.setString(5, Birthdate);
+                insertstatement.setString(6, Email);
+                System.out.println(lineText);
+
+                insertstatement.addBatch();
+                insertstatement.executeBatch();
             }
-            in.close();
-            //insertstatement.executeBatch();
-            }
-            
-    
-    public static boolean compareDni(String lineText, Connection connection) throws SQLException{
-        
+            System.out.println("Total read rows = " + lines);
+        }
+        in.close();
+        // insertstatement.executeBatch();
+    }
+
+    public static boolean compareDni(String lineText, Connection connection) throws SQLException {
+
         List<String> sepList = Arrays.asList(lineText.split(";"));
         List<String> dniList = Utils.selectDNI(connection);
-        
+
         boolean contains = false;
 
-        if(CollectionUtils.containsAny(sepList,dniList)){
-            contains=true;
+        if (CollectionUtils.containsAny(sepList, dniList)) {
+            contains = true;
         }
-    
-        return contains; 
-}
+
+        return contains;
+    }
 }
